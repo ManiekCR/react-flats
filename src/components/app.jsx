860178cec904d18098425df58ea92af2fb15283e
Marian Caron <caron.marian@gmail.com>
2020-09-14 16:22:58 +0200

    add map component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import flats from '../../data/flats.js';
 import FlatList from './flat_list';
+import Marker from './marker';
+
 
 class App extends Component {
   constructor(props) {
@@ -9,13 +12,25 @@ class App extends Component {
     this.flats = { flats };
   }
 
+  static defaultProps = {
+    center: {
+      lat: 48.884211,
+      lng: 2.34689
+    },
+    zoom: 11
+  };
+
   render() {
-    return(
+    return (
       <div>
         <div className="flat-list">
           <FlatList flats={this.flats} />
         </div>
-        <div className="map-container"></div>
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
+            <Marker />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
